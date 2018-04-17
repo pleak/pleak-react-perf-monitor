eb36142951e4688136e785d9954b56e07d1e68c0
baptistelambert <bapthf2@gmail.com>
2018-04-17 09:08:29 +0200

    test(utils): test generic utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import { AVOIDED_PROPERTIES } from './constants';
 export const isNotAvoidedProperty = property =>
   !AVOIDED_PROPERTIES.includes(property);
 
-export const isPropertyValid = (instance, excludes) => property =>
+export const isPropertyValid = (instance, excludes = []) => property =>
   typeof instance[property] === 'function' && !excludes.includes(property);
 
 export const isObject = obj => typeof obj === 'object' && obj !== null;
